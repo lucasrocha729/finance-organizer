e3fe40ae9652e7957296646ccb525844696aa4fd
lucasrocha729 <lucasrocha.0822@gmail.com>
2023-09-04 23:22:13 -0300

    build: create dockerfile to up postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { FinancialTransactionsModule } from './financial-transactions/financial-transactions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [FinancialTransactionsModule],
+  imports: [
+    FinancialTransactionsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
