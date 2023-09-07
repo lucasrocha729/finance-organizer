b2625eb0f60cdda26fcc712a11447727b0b98a2d
lucasrocha729 <lucasrocha.0822@gmail.com>
2023-09-07 00:36:36 -0300

    refactor: remove decorators on entities

---
+++ src/financial-transactions/entities/financial-transactions.entity.ts
@@ -0,0 +1,23 @@
+export class FinancialTransaction {
+  id: string;
+
+  user: string;
+
+  categoryId: string;
+
+  type: string;
+
+  value: number;
+
+  date: string;
+
+  formOfPayment: string;
+
+  comment: string;
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  deletedAt: string;
+}
---
+++ src/user-management/user.entity.ts
@@ -0,0 +1,15 @@
+export class User {
+  id: string;
+
+  name: string;
+
+  login: string;
+
+  password: string;
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  deletedAt: string;
+}
