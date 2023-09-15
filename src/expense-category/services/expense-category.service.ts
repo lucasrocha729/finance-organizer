4bff71b2dc72ec82775ae78fecf3c2331fc04656
lucasrocha729 <lucasrocha.0822@gmail.com>
2023-09-14 21:21:29 -0300

    feat: ensure that duplicate categories are not created

--- src/expense-category/services/expense-category.service.ts
+++ src/expense-category/services/expense-category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { ExpenseCategory } from '../models/expense-category.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateExpenseCategoryDto } from '../dto/create-expense-category.dto';
@@ -12,6 +12,15 @@ export class ExpenseCategoryService {
   ) {}
 
   async registerExpenseCategory(createExpenseCategoryDto: CreateExpenseCategoryDto): Promise<ExpenseCategory> {
+    const name = createExpenseCategoryDto.name.trim().toLocaleLowerCase();
+    const userId = createExpenseCategoryDto.userId;
+
+    const equalCategoryToUser = await this.financialTransactionsRepository.findBy({ name, userId });
+
+    if (equalCategoryToUser.length) {
+      throw new HttpException('Category already existing for the user', HttpStatus.CONFLICT);
+    }
+
     try {
       const expenseCategory = this.financialTransactionsRepository.create(createExpenseCategoryDto);
 
